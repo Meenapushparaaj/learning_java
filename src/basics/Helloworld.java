d18c474dca46742aa2a7a352021af3d1a1db28d0
meena <meenapuhparaajv@gmail.com>
2023-09-14 15:42:16 +0530

    Added some java programe

---
+++ src/basics/Helloworld.java
@@ -0,0 +1,25 @@
+package basics;
+
+//This is my first java program
+/*
+ * This is my firdt java program and its name is Helloworld
+ * 
+ * To compile a java program,
+ * javac filename.java
+ * 
+ * To execute a java program,
+ * java filename
+ */
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+		
+						
+							
+	}
+
+}
