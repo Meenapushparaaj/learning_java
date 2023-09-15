b3f9352146c68b6db69c7995f085d3a2c6c6d004
meena <meenapuhparaajv@gmail.com>
2023-09-15 09:21:04 +0530

    master

---
+++ src/basics/ArithmaticoperaterEx.java
@@ -0,0 +1,40 @@
+package basics;
+
+public class ArithmaticoperaterEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		int d = b-a;
+		int e = a*b;
+		int f = b/a;
+		int g=a%b;
+		
+				
+			System.out.println("The addition of a+b is "+c);
+			
+		
+		System.out.println("The subtraction of b-a is "+d);
+		
+		
+		System.out.println("The multiplication of a*b is "+e);
+
+		
+		System.out.println("The divition of b/a is "+f);
+
+		
+		System.out.println("The modulo of a%b is "+g);
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/basics/DatatypesEx.java
@@ -0,0 +1,56 @@
+package basics;
+
+public class DatatypesEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//primitive data types
+		/*
+		 * byte--> which can store integer value between -128 to 127 
+		 * short --> store integer value from -32,768 to 32,767
+		 * int --> 2^31 to 2^31-1
+		 * long --> -2^63 to 2^63-1
+		 * (this above four is used to store integer value only)
+		 * 
+		 * float --> storing decimal nos.It can store 7 digits after decimal
+		 * double --> storing decimal nos.It can store 16 digit after decimal
+		 * boolean --> true or false
+		 * char --> to store single character(single alphabet)
+		 * 
+		 * 
+		 * int--> what type of date to store in the variable
+		 * a--> variable name
+		 * 10--> value
+		 * 
+		 * Memory for each datatype
+		 * byte- 1 byte (8 bits)
+		 * short- 2 bytes(16 bits)
+		 * int- 4 bytes
+		 * long- 8 bytes
+		 * double- 8 bytes
+		 * float- 4 bytes
+		 * boolean- 1 bit
+		 * char- 2 bytes
+		 */
+		
+		
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println("The value of a is "+c);
+		
+	//concatenation is combining 2 different data types
+		
+				
+				
+				
+				
+		
+		/*
+		 * int-->what type of data to store in the variable
+		 * a-->
+		 */
+	}
+
+}
